275c9543bd0a8c20e7f48903629ab710a63a5734
NaveenSolanki8319 <naveen.solanki8319@gmail.com>
2023-02-18 08:54:08 +0530

    Random Task 17 Feb

---
+++ Random Task 17 Feb/Palindrome.java
@@ -0,0 +1,50 @@
+package random;
+import java.util.*;
+
+public class Palindrome 
+{
+	public static void main(String []args)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter a No. :- ");
+		int num=sc.nextInt();
+		int res=0;
+		if(num%2==0)
+		{
+			num=num/2;
+			int i=1;
+			while(i<=num)
+			{
+				res=(res*10)+i;
+				i++;
+			}
+			i--;
+			while(i>=1)
+			{
+				res=(res*10)+i;
+				i--;
+			}
+		}
+		else
+		{
+			num=num/2;
+			num++;
+			System.out.println("num "+ num);	
+			int i=1;
+			while(i<=num)
+			{
+				res=(res*10)+i;
+				i++;
+			}
+			i=i-2;
+			while(i>=1)
+			{
+				res=(res*10)+i;
+				i--;
+			}
+		}	
+		System.out.println("Result :- "+res);	
+		sc.close();
+	}
+
+}
