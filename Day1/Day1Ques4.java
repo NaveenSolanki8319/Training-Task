fe5906bff9425bb44198956ba9ac9c654466fa40
NaveenSolanki8319 <naveen.solanki8319@gmail.com>
2023-02-18 08:48:48 +0530

    Day 1 Task

---
+++ Day1/Day1Ques4.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Day1Ques4 
+{
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		String name;
+		System.out.print("Enter Your name :- ");
+		name=sc.nextLine();
+		System.out.println("Hello");
+		System.out.println(name);	
+		sc.close();
+	}
+
+}
