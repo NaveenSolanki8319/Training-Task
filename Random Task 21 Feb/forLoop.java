0024c6a8944db1cd4d46c197e0917dcd62487c38
NaveenSolanki8319 <naveen.solanki8319@gmail.com>
2023-02-22 16:23:25 +0530

    Assignment 2 Random Task

---
+++ Random Task 21 Feb/forLoop.java
@@ -0,0 +1,18 @@
+import java.util.Date;
+
+public class forLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double startTime= new Date().getTime();
+		double i;
+		for(i=-999999999;i<999999999;i++)
+		{
+		}
+		double endTime= new Date().getTime();
+		double ans=endTime-startTime;
+		System.out.print(ans);
+		
+	}
+
+}
